9a34b20919dca4b622850edf5ef19f642cf32be6
thebailers <5601214+thebailers@users.noreply.github.com>
2021-01-28 12:01:44 +0000

    late refunds

--- react/src/components/Home.tsx
+++ react/src/components/Home.tsx
@@ -21,24 +21,24 @@ export default function Home() {
               Class 1 NI calculators
             </h2>
             <ul className="govuk-list govuk-list--bullet">
-              <li><Link to="/class-1">Calculate Class 1 National Insurance (NI) contributions</Link></li>
-              <li><Link to="/directors">Directors’ contributions</Link></li>
-              <li><Link to="/unofficial-deferment">Class 1 NI contributions an employer owes due to unofficial
+              <li><Link to="/class-1" className="govuk-link">Calculate Class 1 National Insurance (NI) contributions</Link></li>
+              <li><Link to="/directors" className="govuk-link">Directors’ contributions</Link></li>
+              <li><Link to="/unofficial-deferment" className="govuk-link">Class 1 NI contributions an employer owes due to unofficial
                 deferment</Link></li>
             </ul>
             <h2 className="govuk-heading-m">
               Class 2 and 3 NI calculators
             </h2>
             <ul className="govuk-list govuk-list--bullet">
-              <li><Link to="/class-2-or-3">Class 2 or 3 NI contributions needed for a qualifying year</Link></li>
-              <li><Link to="/class-3">Weekly contribution conversion</Link></li>
+              <li><Link to="/class-2-or-3" className="govuk-link">Class 2 or 3 NI contributions needed for a qualifying year</Link></li>
+              <li><Link to="/class-3" className="govuk-link">Weekly contribution conversion</Link></li>
             </ul>
             <h2 className="govuk-heading-m">
               Interest calculators
             </h2>
             <ul className="govuk-list govuk-list--bullet">
-              <li><Link to="/late-interest">Interest on late or unpaid Class 1 NI contributions</Link></li>
-              <li><Link to="/interest-refunds">Interest on late-paid refunds from 1993 to 1994</Link></li>
+              <li><Link to="/late-interest" className="govuk-link">Interest on late or unpaid Class 1 NI contributions</Link></li>
+              <li><Link to="/late-refunds" className="govuk-link">Interest on late-paid refunds from 1993 to 1994</Link></li>
             </ul>
           </nav>
         </>
--- react/src/interfaces/index.tsx
+++ react/src/interfaces/index.tsx
@@ -106,6 +106,14 @@ export interface Class1DebtRow {
   interestDue: string | null
 }
 
+export interface LateRefundsTableRowProps {
+  id: string
+  taxYears: TaxYear[]
+  taxYear: TaxYear
+  refund: string,
+  payable: string
+}
+
 export interface TableProps {
   showBands: boolean;
   printView: boolean
@@ -150,9 +158,8 @@ export interface Class12Or3SavePrintProps extends SavePrintBaseProps {
   result: Class2Or3Result | null
 }
 
-export interface LateInterestPrintProps extends SavePrintBaseProps {
-
-}
+export interface LateInterestPrintProps extends SavePrintBaseProps {}
+export interface LateRefundPrintProps extends SavePrintBaseProps {}
 
 export interface CategoryTotalsProps {
   rows: Array<Row | DirectorsRow>
@@ -179,6 +186,7 @@ export interface TextInputProps {
   inputMode?: "numeric"
   onChangeCallback: React.ChangeEventHandler<HTMLInputElement>
   onBlurCallback?: React.ChangeEventHandler<HTMLInputElement>
+  error?: any // todo
 }
 
 export interface GovDateRange {
@@ -207,4 +215,9 @@ export enum TotalType {
   EE = 'ee',
   ER = 'er',
   GROSS = 'gross'
+}
+
+export interface Rate {
+  year: number
+  rate: number
 }
