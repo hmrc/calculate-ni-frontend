0938deda70646e900aa196bc7da8bbec78748741
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-02-08 11:10:47 +0000

    fix date conversion (out by one month) (#78)

--- frontend/src/main/scala/package.scala
+++ frontend/src/main/scala/package.scala
@@ -5,9 +5,9 @@ import java.time.LocalDate
 
 package object frontend {
   implicit def convertDate(in: js.Date): LocalDate =
-    LocalDate.of(in.getFullYear.toInt, in.getMonth.toInt, in.getDate.toInt)
+    LocalDate.of(in.getFullYear.toInt, in.getMonth.toInt + 1, in.getDate.toInt)
 
   implicit def convertDateBack(in: LocalDate): js.Date =
-    new js.Date(in.getYear, in.getMonthValue, in.getDayOfMonth)
+    new js.Date(in.getYear, in.getMonthValue - 1, in.getDayOfMonth)
 
 }
