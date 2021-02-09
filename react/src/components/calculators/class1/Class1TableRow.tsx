76fe807d087048b4ee271fb688f940af53d0d5c5
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2021-02-09 16:17:39 +0000

    removed optional value from select

--- react/src/components/calculators/class1/Class1TableRow.tsx
+++ react/src/components/calculators/class1/Class1TableRow.tsx
@@ -99,7 +99,7 @@ export default function Class1TableRow(props: TableRowProps) {
           :
           <>
             <label className="govuk-visually-hidden" htmlFor={`row${index}-category`}>Category</label>
-            <select name="category" value={row.category || categories[0]} onChange={(e) => handleSelectChange?.(row, e)} className="borderless" id={`row${index}-category`}>
+            <select name="category" value={row.category} onChange={(e) => handleSelectChange?.(row, e)} className="borderless" id={`row${index}-category`}>
               {categories.map((c: string, i: number) => (
                 <option key={i} value={c}>
                   {`${c}${config.categoryNames[c] ? ` - ${config.categoryNames[c]}` : ``}`}
