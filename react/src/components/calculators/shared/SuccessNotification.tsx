12a044073a0b2a8bb7df298b86827ddf2f22bc66
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2021-02-18 16:21:33 +0000

    labelled notification with more detail

--- react/src/components/calculators/shared/SuccessNotification.tsx
+++ react/src/components/calculators/shared/SuccessNotification.tsx
@@ -7,15 +7,14 @@ export function SuccessNotification(props: {table?: boolean, totals?: boolean}) 
          aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
       <div className="govuk-notification-banner__header">
         <h2 className="govuk-notification-banner__title" id="govuk-notification-banner-title">
-          Success
+          The page has been updated with the results of your calculation
         </h2>
       </div>
       <div className="govuk-notification-banner__content">
-        <p className="govuk-body">
-          The page has been updated with the results of your calculation
-          {table && <><br/><a href="#results-table">Skip to results in table</a></>}
-          {totals && <><br/><a href="#results-totals">Skip to results totals</a></>}
-        </p>
+        <ul className="govuk-list">
+          {table && <li><a href="#results-table">Skip to results in table</a></li>}
+          {totals && <li><a href="#results-totals">Skip to results totals</a></li>}
+        </ul>
       </div>
     </div>
   )
