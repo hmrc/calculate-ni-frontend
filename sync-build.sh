3e09e61205bd39b39226c53b406262d3078f778a
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2021-02-24 10:35:53 +0000

    remove sed ops from bash script and add homepage path

--- sync-build.sh
+++ sync-build.sh
@@ -5,12 +5,3 @@ export LC_CTYPE=LC_ALL
 rm app/assets/precache-manifest.*
 rm -rf app/assets/static
 rsync -av react/build/ app/assets/
-
-sed -i'' -e 's|href="/|href="/calculate-ni/|g' -e 's|src="/|src="/calculate-ni/|g' app/assets/index.html
-
-sed -i'' -e 's|": "/|": "/calculate-ni/|' app/assets/asset-manifest.json
-
-sed -i'' -e 's|"static|"calculate-ni/static|g' app/assets/static/js/*.js
-
-sed -i'' -e 's|url(/|url(/calculate-ni/|g' app/assets/static/css/*.css
-
