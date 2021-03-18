35679f638a9b27b90141dd94dc32e2e0445b0d44
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-03-18 13:54:45 +0000

    remove old frontend facades

--- frontend/src/main/scala/Frontend.scala
+++ frontend/src/main/scala/Frontend.scala
@@ -19,7 +19,6 @@ class NiFrontend(json: String) extends js.Object {
   lazy val classOne = new ClassOneFrontend(config)
   lazy val classTwo = new ClassTwoAndThreeFrontend(config.classTwo)
   lazy val classThree = new ClassTwoAndThreeFrontend(config.classThree)
-  lazy val classFour = new ClassFourFrontend(config)
   lazy val weeklyContributions = new WeeklyContributions(config)
   lazy val interestOnLateClassOne = new InterestOnUnpaidFrontend(config)
   lazy val interestOnRefundsClassOne = new InterestOnRefundsClassOne(config)
