6f85492f24fd915eb808623d1b1a5eb4d9cf7ae3
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-04-12 15:17:21 +0100

    add in yearly breakdown to facade

--- frontend/src/main/scala/WeeklyContributions.scala
+++ frontend/src/main/scala/WeeklyContributions.scala
@@ -13,4 +13,28 @@ class WeeklyContributions(
     to: js.Date
   ): Int = Interval[java.time.LocalDate](from, to).numberOfTaxWeeks.get
 
+  def breakdown(
+    from: js.Date,
+    to: js.Date
+  ): js.Object = {
+    val r = Interval[java.time.LocalDate](from, to).taxWeekBreakdown
+    val yearsSeq = r.map {
+      case (y, w) =>
+
+        new js.Object {
+          val weeks: Int = w
+          val year: Int = y.startingYear
+
+          val startDate: js.Date = y.start
+          val endDate: js.Date = y.end
+        }
+    }
+    val total = r.map(_._2).sum
+
+    new js.Object {
+      val totalWeeks = total
+      val years = yearsSeq
+    }
+  }
+
 }
