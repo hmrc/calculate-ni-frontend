ffbf727314dfde7ddce2e99b010bdd83dc0d0c54
thebailers <5601214+thebailers@users.noreply.github.com>
2021-04-16 11:49:56 +0100

    WIP fix annual breakdown print styles

--- react/src/components/calculators/class-3/Class3Breakdown.tsx
+++ react/src/components/calculators/class-3/Class3Breakdown.tsx
@@ -11,7 +11,7 @@ export default function Class3Breakdown(props: Class3BreakdownProps) {
   const { results, isSaveAndPrint } = props
   return (
   <div className={`${isSaveAndPrint ? `save-print-wrapper ` : ``}section--bottom section-outer--bottom`}>
-    <div className="table-wrapper">
+    <div className={`${isSaveAndPrint ? `contribution-details` : `table-wrapper`}`}>
       <h2 className="section-heading">Results breakdown</h2>
 
       <table className="shade-rows section-outer--top" id="results-table">
